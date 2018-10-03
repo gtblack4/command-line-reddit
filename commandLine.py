7a5cb0cafc98bd14edae5d65f9de4d0246bf0aba
Gage Black <gblack@aschulman.com>
2018-10-03 11:54:08 -0400

    +Improved comment formatting +When there are no more comments it returns to the menu +Added a "Re-Display Posts" option to the subreddit Menu
    Signed-off-by: Gage Black <gtblack4@gmail.com>

--- commandLine.py
+++ commandLine.py
@@ -2,14 +2,45 @@ import praw
 from praw.models import MoreComments
 import textwrap
 import math
+import getpass
 #Session Information -- Please Don't steal my account
-reddit = praw.Reddit(client_id='0rDPWpdN0p5rnA',
-	client_secret='FIKkxfmqKdlst2etbot6pjaLtf8',
-	password='XemogDev',
-	user_agent='test scripts',
-	username='Xemog_Dev')
+	#client_id='0rDPWpdN0p5rnA
+	#client_secret='FIKkxfmqKdlst2etbot6pjaLtf8',
+	#password='XemogDev',
+	#username='Xemog_Dev')
 #viewSubmission displays the top 20 comments using the user selection
 #This function needs work, but I'm not sure the direction to take it in yet
+loggedIn = False
+reddit = praw.Reddit(client_id='0rDPWpdN0p5rnA',
+	client_secret='FIKkxfmqKdlst2etbot6pjaLtf8',
+	user_agent='test scripts',
+	password='XemogDev',
+	username='Xemog_Dev')
+def Main():
+	
+	menu = {}
+	menu['1']="Homepage"
+	menu['2']="Choose Subreddit"
+	menu['3']="Quit"
+	while True:
+		options=menu.keys()
+		print(" ")
+		for entry in options:	
+			print(entry, menu[entry])
+		print(" ")
+		menuSelect = input("Select an Option:")
+		if menuSelect =='1':
+			chooseSubreddit('hot',"all")
+		if menuSelect =='2':
+			chooseSubreddit('hot'," ")
+		if menuSelect =='3':
+			break
+
+def printParentNewline():
+	print('{0:6}|{1:9}|{2:100.100}'.format('______','_________','_________________________________________________________________________________________________________'))
+
+def leaveComment():
+	print("lravecomment")
 def displayReplies(replies,depth):
 	varY = 1
 	depthHolder = ""
@@ -30,74 +61,77 @@ def displayReplies(replies,depth):
 
 	while varY <= (math.ceil(len(commentBody)/100)):
 		if varY == 1:
-			print('{0:6}{1:5}|{2:9.9}|{3}'.format(depthHolder,replies.score,replies.author.name,str(commentBody[(varY-1)*100:varY*100])))
+			print('{0:6}{1:5}|{2:9.9}|{3}'.format(depthHolder,replies.score,replies.author.name,str(commentBody[(varY-1)*(100-((depth+1)*5)):varY*(100-((depth+1)*5))])))
 		else:
-			print('{0:6}{1:5}|{2:9.9}|{3}'.format(depthHolder,"     "," ",str(commentBody[(varY-1)*100:varY*100])))
+			print('{0:6}{1:5}|{2:9.9}|{3}'.format(depthHolder,"     "," ",str(commentBody[(varY-1)*(100-((depth+1)*5)):varY*(100-((depth+1)*5))])))
 		varY = varY + 1
 	print('{0:6}{1:5}|{2:9}|{3}'.format(depthHolder,'     ','_________',widthHolder))
-def viewSubmission(subId,chosenSubreddit):
+	
+def displayLastComment(lineCount):
+	print("This is under construction")
+def viewSubmission(subId,chosenSubreddit,sortBy):
 	submission = reddit.submission(id=subId)
-	commentCount = 0
-	print('{0:6}|{1:9}|{2:100.100}'.format("Score","Author","Comment"))
-	lineCount = 0
-	while True:
-		for comment in submission.comments:
-			if isinstance(comment, MoreComments):
+	print('{0:6}|{1:9}|{2:10.10}|{3:11}{4:89.89}'.format("Score","Author","Comment","Post Title:",submission.title))
+	lineCount = 1
+	x = 1
+	for comment in submission.comments:
+		if isinstance(comment, MoreComments):
+			continue
+		lineCount = lineCount + 1
+		body = str(comment.body).replace('\n','')
+		varX = 1
+		commentNum = 1
+		#This logic splits comments longer than 100 characters into multiples lines
+		#It allows us to keep the formatting without longer comments running onto multiples lines
+		#
+		print('{0:6}.{1:9}.{2:100.100}'.format('______','_________','_________________________________________________________________________________________________________'))
+		while varX <= (math.ceil(len(body)/100)):
+			if varX == 1:
+				print('{0:6}|{1:9.9}|{2:100.100}'.format(comment.score,comment.author.name,str(body[(varX-1)*100:varX*100])))
+			else:
+				print('{0:6}|{1:9}|{2:100}'.format(" "," ",str(body[(varX-1)*100:varX*100])))
+			varX = varX + 1
+			
+		printParentNewline()#this is 120 characters long
+		print(commentNum)
+		
+		for replies in comment.replies:
+			depth = 1
+			if isinstance(replies, MoreComments):
 				continue
-			body = str(comment.body).replace('\n','')
-			varX = 1
-			#This logic splits comments longer than 100 characters into multiples lines
-			#It allows us to keep the formatting without longer comments running onto multiples lines
-			#
-			
-			while varX <= (math.ceil(len(body)/100)):
-				if varX == 1:
-					print('{0:6}|{1:9}|{2:100.100}'.format(comment.score,comment.author.name,str(body[(varX-1)*100:varX*100])))
-				else:
-					print('{0:6}|{1:9}|{2:100}'.format(" "," ",str(body[(varX-1)*100:varX*100])))
-				varX = varX + 1
-				commentCount = commentCount + 1
-			print('{0:6}|{1:9}|{2:100.100}'.format('______','_________','_________________________________________________________________________________________________________'))#this is 120 characters long
-			lineCount = lineCount + 1
-			if lineCount%20==0:
-				userNext = input("Enter 'Next' to view the next 20 comments or 'menu' to return to the Subreddit homepage:")
-				if userNext == 'menu':
-					break
-			
-			for replies in comment.replies:
-				depth = 1
-				if isinstance(replies, MoreComments):
+			commentNum = commentNum + 1 
+			displayReplies(replies,depth)
+			for children in replies.replies:
+				depth = 2
+				if isinstance(children, MoreComments):
 					continue
-				displayReplies(replies,depth)
-				for children in replies.replies:
-					depth = 2
-					if isinstance(children, MoreComments):
+				commentNum = commentNum + 1 
+				displayReplies(children,depth)
+				for toddler in children.replies:
+					depth = 3
+					if isinstance(toddler, MoreComments):
 						continue
-					displayReplies(children,depth)
-					for toddler in children.replies:
-						depth = 3
-						if isinstance(toddler, MoreComments):
+					commentNum = commentNum + 1 
+					displayReplies(toddler,depth)
+					for baby in toddler.replies:
+						depth = 4
+						if isinstance(baby, MoreComments):
 							continue
-						displayReplies(toddler,depth)
-						for baby in toddler.replies:
-							depth = 4
-							if isinstance(baby, MoreComments):
-								continue
-							displayReplies(baby,depth)
-
+						commentNum = commentNum + 1 
+						displayReplies(baby,depth)
+			print(" ")
+			userNext = input("'Enter','Back','Menu','Comment':")
+			if userNext == 'Menu':
+				chooseSubreddit(sortBy,chosenSubreddit)
+			if userNext == 'Next':
+				continue
+			if userNext == 'Back':
+				displayLastComment(lineCount)
+			if userNext == 'Comment':
+				leaveComment()
+			lineCount = 1
+			x = x + 1
 	print(" ")
-	# while True:
-	# 	chosenComment = input('{0}{1}{2}'.format("Select a parent comment to view children, or enter 'Back' to return to the ",chosenSubreddit," homepage :"))
-	# 	commentCounter = 1 
-	# 	if chosenComment == 'Back':
-	# 		break
-	# 	for top_level_comment in submission.comments:
-	# 		if chosenComment == commentCounter:
-	# 			viewChildComment()
-	# 			print(top_level_comment.body)
-	# 		commentCounter = commentCounter + 1
-
-
 
 #chooseSubreddit displays the top 20 posts based on the user entry.
 def chooseSubreddit(sortBy,chosenSubreddit):
@@ -126,6 +160,7 @@ def chooseSubreddit(sortBy,chosenSubreddit):
 		else:
 			menu['2']="Change Sort By to: Hot"
 		menu['3']="Choose a Different Subreddit"
+		menu['4']="Re-Display Posts"
 
 		while True:
 			options=menu.keys()
@@ -145,12 +180,12 @@ def chooseSubreddit(sortBy,chosenSubreddit):
 				if sortBy == 'hot':	
 					for submission in reddit.subreddit(chosenSubreddit).hot(limit=chosenSubmission):
 						if submissionCount == chosenSubmission:
-							viewSubmission(submission.id,chosenSubreddit)
+							viewSubmission(submission.id,chosenSubreddit,sortBy)
 						submissionCount = submissionCount + 1
 				if sortBy == 'top':	
 					for submission in reddit.subreddit(chosenSubreddit).top(limit=chosenSubmission):	
 						if submissionCount == chosenSubmission:
-							viewSubmission(submission.id,chosenSubreddit)
+							viewSubmission(submission.id,chosenSubreddit,sortBy)
 						submissionCount = submissionCount + 1
 				
 			if menuSelect =='2':
@@ -161,34 +196,10 @@ def chooseSubreddit(sortBy,chosenSubreddit):
 			if menuSelect == '3':
 				chosenSubreddit =""
 				chooseSubreddit(sortBy," ")
-			#chosenSubmission = input("Enter the submission number you want to view, or enter 'exit' to return to the menu:")
-			# if chosenSubmission =='exit':
-			# 	break
-			# chosenSubmission = int(chosenSubmission)
-			# submissionCount = 1
-			# for submission in reddit.subreddit(chosenSubreddit).hot(limit=chosenSubmission):
-			# 	if submissionCount == chosenSubmission:
-			# 		viewSubmission(submission.id,chosenSubreddit)
-			# 	submissionCount = submissionCount + 1
+			if menuSelect == '4':
+				chooseSubreddit(sortBy,chosenSubreddit)
+Main()
 
-#This is the Main menu
-menu = {}
-menu['1']="Homepage"
-menu['2']="Choose Subreddit"
-menu['3']="Quit"
-while True:
-	options=menu.keys()
-	print(" ")
-	for entry in options:	
-		print(entry, menu[entry])
-	print(" ")
-	menuSelect = input("Select an Option:")
-	if menuSelect =='1':
-		chooseSubreddit('hot',"all")
-	if menuSelect =='2':
-		chooseSubreddit('hot'," ")
-	if menuSelect =='3':
-		break
 
 
 
